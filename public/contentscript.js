051ba3f7d9711ba78915957856350cfb88d64ca4
Pedro Magno Müller <pm.mueller@pragmaticindustries.de>
2022-11-03 20:27:44 +0100

    (task-2)hover and tooltip with dummy info was added to the element

--- public/contentscript.js
+++ public/contentscript.js
@@ -1,5 +1,51 @@
 console.log('Contentscript injected');
 
+// https://stackoverflow.com/questions/15505225/inject-css-stylesheet-as-string-using-javascript
+// Used to inject the CSS stylesheet
+function addStyle(styleString) {
+  const style = document.createElement('style');
+  style.textContent = styleString;
+  document.head.append(style);
+}
+
+//https://www.w3schools.com/css/tryit.asp?filename=trycss_tooltip
+// Used to inject the tooltip
+addStyle(`
+    .budget_to_beat {
+        display: flex;
+        float: right;
+        justify-content: space-between;
+        color: white;
+        margin: auto;
+        background-color: darkBlue;
+        text-align: center;
+        border-radius: 15px;
+        padding: 20px 50px;
+        font-size: 20px;
+        }
+
+      .budget_to_beat:hover {
+          background-color: blue; 
+      }
+
+      .budget_to_beat .tooltip {
+        visibility: hidden;
+        width: 120px;
+        background-color: black;
+        color: #fff;
+        text-align: center;
+        padding: 5px 0;
+        border-radius: 6px;
+       
+        /* Position the tooltip text - see examples below! */
+        position: absolute;
+      }
+
+      .budget_to_beat:hover .tooltip {
+        visibility: visible;
+      }
+  `);
+
 // Get the Budget-to-beat value from the page
 const euroValue = document.querySelector(
   '#root > div > div > div.makeStyles-mainPanel-3 > div.makeStyles-scrollbars-5 > div:nth-child(1) > div > section > div:nth-child(3) > p.makeStyles-btb-27',
@@ -8,27 +54,22 @@ const euroValue = document.querySelector(
 console.log(euroValue);
 
 // Create the element to display the value
-let element = document.createElement('div');
+let tooltip = document.createElement('div');
+let element = document.createElement('span');
 
+// Add the class to the element
+element.classList.add('budget_to_beat');
+tooltip.classList.add('tooltip');
+
+// Add the value to the element
 const img = document.createElement('img');
 img.src = '/images/favicon-32x32.png';
-
-const injectedText = document.createTextNode(`Budget-to-Beat: ${euroValue}`);
-
+const injectedText_1 = document.createTextNode(`Budget-to-Beat: ${euroValue}`);
+const injectedText_2 = document.createTextNode(`Dummy Data on Climate Change`);
 element.appendChild(img);
-element.appendChild(injectedText);
-
-// Style the element
-element.style.display = 'flex';
-element.style.float = 'right';
-element.style.justify = 'space-evenly';
-element.style.color = 'white';
-element.style.margin = 'auto';
-element.style.backgroundColor = 'darkblue';
-element.style.textAlign = 'center';
-element.style.borderRadius = '15px';
-element.style.padding = '20px 50px';
-element.style.fontSize = '20px';
+element.appendChild(injectedText_1);
+element.appendChild(tooltip);
+tooltip.appendChild(injectedText_2);
 
 // Add the element to the page
 const parentElement = document.querySelector(
